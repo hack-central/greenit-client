443bb815bd06826df951136ff2fb4f8d75f4106f
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-01-17 00:38:28 +0530

    feat: profile route added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './layouts/Home';
+import Profile from './layouts/Profile';
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
         </Route>
         <Route exact path="/events">
           {/* <Events /> */}
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
         </Route>
       </Switch>
     </Router>
