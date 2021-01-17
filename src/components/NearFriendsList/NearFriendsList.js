cc6a510733d8f711c91a914417262a5bc9a1bbaa
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-01-17 21:09:24 +0530

    feat: nearFriends list

---
+++ src/components/NearFriendsList/NearFriendsList.js
@@ -0,0 +1,47 @@
+import { List, Typography, Empty, Card } from 'antd';
+import { useEffect, useState } from 'react';
+import SuggestedUser from '../SuggestedUser/SuggestedUser';
+
+const { Title } = Typography;
+
+export default function NearFriendsList() {
+  const [users, setResponse] = useState([]);
+
+  useEffect(() => {
+    const fetchReq = async () => {
+      const usersRes = await fetch('https://earthy.sauravmh.com/api/users');
+      const usersData = await usersRes.json();
+      setResponse(usersData);
+    };
+
+    fetchReq();
+  }, []);
+
+  const GenerateUsers = () => {
+    if (users.length <= 0) {
+      return <Empty />;
+    }
+
+    return (
+      <List
+        itemLayout="vertical"
+        size="large"
+        dataSource={users.slice(5, 10)}
+        renderItem={(user) => (
+          <SuggestedUser
+            avatarurl={user.avatar}
+            username={user.firstName}
+            key={user.id}
+          />
+        )}
+      />
+    );
+  };
+
+  return (
+    <Card style={{ marginTop: 16 }}>
+      <Title level={4}>People you may know</Title>
+      <GenerateUsers />
+    </Card>
+  );
+}
