1995dbbf61f63edf56e07c8016bcf26354a4e292
Kartik Choudhary <kartikc.918@gmail.com>
2021-01-17 19:54:23 +0530

    feat: redo event details layout and events page

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import {
-  Layout,
   Card,
   Divider,
   Row,
@@ -8,22 +7,23 @@ import {
   Modal,
   Typography,
   Avatar,
+  Steps,
+  Carousel,
 } from 'antd';
-import {
-  EnvironmentOutlined,
-  ClockCircleOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
+import { EnvironmentOutlined, CalendarOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
-const { Text } = Typography;
+const { Title, Text } = Typography;
+const { Step } = Steps;
 
 export default function Event({
+  eventName,
   eventImage,
   eventLocation,
   eventDate,
   eventOrganizer,
   eventOrganizerImage,
+  eventDescription,
 }) {
   const [visible, setVisible] = useState(false);
 
@@ -36,12 +36,14 @@ export default function Event({
         onClick={() => setVisible(true)}
       >
         <Row>
-          <Col span={8}>
+          <Col span={9}>
             <img alt="example" src={eventImage} />
           </Col>
-          <Col span={16}>
-            <Meta title="Event Name" description="Location" />
-            <br />
+          <Col span={12}>
+            <Meta
+              title={`${eventName}`}
+              description={`${eventLocation}, ${eventDate}`}
+            />
             <br />
             <Row style={{ marginTop: '5px' }}>
               <Avatar size="large" src={eventOrganizerImage} />
@@ -69,44 +71,67 @@ export default function Event({
             />
           </Col>
           <Col span={17}>
-            <Text className="modalTitle">Event Name</Text>
-            <br></br>
-            <Avatar
-              className="userImg"
-              style={{ backgroundColor: '#87d068' }}
-              icon={<UserOutlined />}
-            />
-            <Text type="success" className="user">
-              UserName
-            </Text>
-            <br></br>
+            <Text className="modalTitle">{eventName}</Text>
+            <br />
             <Text type="secondary" className="modalSubTitle">
-              <EnvironmentOutlined />
-              Location<br></br>
-              <ClockCircleOutlined />
-              Time
+              <EnvironmentOutlined style={{ marginRight: '10px' }} />
+              {eventLocation}
+              <br />
+              <CalendarOutlined style={{ marginRight: '10px' }} />
+              {eventDate}
             </Text>
+
+            <Row style={{ marginTop: '5px' }}>
+              <Avatar size="large" src={eventOrganizerImage} />
+              <p style={{ paddingLeft: '10px', margin: 'auto 0' }}>
+                {eventOrganizer}
+              </p>
+            </Row>
           </Col>
         </Row>
         <Divider dashed />
-        <Text>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </Text>
+        <Row>
+          <Text>{eventDescription}</Text>
+        </Row>
+        <Row justify="center">
+          <Col span={8}>
+            <Steps
+              direction="vertical"
+              size="small"
+              current={1}
+              style={{ width: '100%', marginTop: 32 }}
+            >
+              <Step title="Event Confirmed" />
+              <Step
+                title="Awaiting Registrations"
+                description="Open till 20-01-2021"
+              />
+              <Step title="Event Complete" />
+            </Steps>
+          </Col>
+          <Col span={8}>
+            <Card style={{ marginTop: 32 }}>
+              <Title level={4}>See on Maps</Title>
+            </Card>
+          </Col>
+        </Row>
       </Modal>
     </>
   );
 }
 
 Event.defaultProps = {
+  eventName: 'Event Name',
   eventOrganizer: 'Username',
+  eventDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting
+  industry. Lorem Ipsum has been the industry's standard dummy text ever
+  since the 1500s, when an unknown printer took a galley of type and
+  scrambled it to make a type specimen book. It has survived not only
+  five centuries, but also the leap into electronic typesetting,
+  remaining essentially unchanged. It was popularised in the 1960s with
+  the release of Letraset sheets containing Lorem Ipsum passages, and
+  more recently with desktop publishing software like Aldus PageMaker
+  including versions of Lorem Ipsum.`,
   eventOrganizerImage:
     'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
   eventImage:
--- src/layouts/Events.js
+++ src/layouts/Events.js
@@ -8,30 +8,89 @@ import {
   Modal,
   Typography,
   Avatar,
+  Button,
   Input,
   AutoComplete,
 } from 'antd';
-import {
-  EnvironmentOutlined,
-  ClockCircleOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
+import { PlusSquareOutlined } from '@ant-design/icons';
 import Navbar from '../components/Navbar/Navbar';
 import Event from '../components/Event/Event';
 import PageFooter from '../components/PageFooter/PageFooter';
 
-const { Meta } = Card;
-const { Text } = Typography;
+import mapBg from '../assets/mapbg.png';
+
 const { Sider, Content } = Layout;
 
+const getRandomInt = (max, min = 0) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const searchResult = (query) => {
+  return new Array(getRandomInt(5))
+    .join('.')
+    .split('.')
+    .map((_, idx) => {
+      const category = `${query}${idx}`;
+      return {
+        value: category,
+        label: (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <span>
+              Found {query} on{' '}
+              <a
+                href={`https://s.taobao.com/search?q=${query}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {category}
+              </a>
+            </span>
+            <span>{getRandomInt(200, 100)} results</span>
+          </div>
+        ),
+      };
+    });
+};
+
 export default function Events() {
+  const [options, setOptions] = useState([]);
+
+  const handleSearch = (value) => {
+    setOptions(value ? searchResult(value) : []);
+  };
+
+  const onSelect = (value) => {
+    console.log('onSelect', value);
+  };
+
   return (
     <Layout>
       <Navbar />
 
-      <Layout style={{ maxHeight: '80vh', overflow: 'scroll' }}>
+      <Layout style={{ maxHeight: '80vh', overflowY: 'scroll', overflowX: 'hidden', marginTop: '10px' }}>
         <Content>
           <Col className="event-col">
+            <AutoComplete
+              dropdownMatchSelectWidth={252}
+              style={{
+                width: '80%',
+                marginBottom: '20px',
+              }}
+              options={options}
+              onSelect={onSelect}
+              onSearch={handleSearch}
+            >
+              <Input.Search
+                size="large"
+                placeholder="Search for an event or place"
+                enterButton
+              />
+            </AutoComplete>
             <Event />
             <Event />
             <Event />
@@ -40,8 +99,8 @@ export default function Events() {
             <Event />
           </Col>
         </Content>
-        <Sider className="event-sidebar" width={500}>
-          Sider
+        <Sider className="event-sidebar" width="40%">
+          <img className="event-map-image" alt='map' src={mapBg} />
         </Sider>
       </Layout>
       <PageFooter />
